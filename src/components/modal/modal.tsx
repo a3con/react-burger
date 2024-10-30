94609f270bb2dbcc800a65de169337c917397aaa
a3con <a3con.noreply@gmail.com>
2024-10-30 13:29:05 +0300

    Improve modal
    * Added a container for modal to index.html
    * Moved usaEscapeKey to the directory with hooks
    * Fixed KeyboardEvent type for usaEscapeKey

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useCallback } from 'react'
 import { createPortal } from 'react-dom'
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import style from './modal.module.scss'
 import { ModalOverlay } from './modal-overlay/modal-overlay'
+import { useEscapeKey } from '../../hooks/useEscapeKey'
 
 interface IModalProps {
   children: React.ReactNode
@@ -10,44 +10,24 @@ interface IModalProps {
   title?: string
 }
 
-const KEY_NAME_ESC = 'Escape'
-const KEY_EVENT_TYPE = 'keyup'
-
-function useEscapeKey(handleClose: () => void) {
-  const handleEscKey = useCallback(
-    ({ key }: { key: string }) => {
-      if (key === KEY_NAME_ESC) {
-        handleClose()
-      }
-    },
-    [handleClose],
-  )
-
-  useEffect(() => {
-    document.addEventListener(KEY_EVENT_TYPE, handleEscKey, false)
-
-    return () => {
-      document.removeEventListener(KEY_EVENT_TYPE, handleEscKey, false)
-    }
-  }, [handleEscKey])
-}
-
 export const Modal = ({ children, onClose, title }: IModalProps) => {
+  const modals = document.getElementById('modals') as HTMLElement
+
   useEscapeKey(onClose)
 
   return createPortal(
     <div className={style.modal}>
       <ModalOverlay onClose={onClose} />
       <div className={style.modal__body}>
-        <header className={style.modal__header}>
+        <div className={style.modal__header}>
           <h3 className={style.modal__title}>{title}</h3>
           <button className={style.modal__close} onClick={onClose}>
             <CloseIcon type="primary" />
           </button>
-        </header>
+        </div>
         <div className={style.modal__content}>{children}</div>
       </div>
     </div>,
-    document.body,
+    modals,
   )
 }
