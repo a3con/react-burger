909b98d298217fcc993aee00e84ccfb04333f02b
a3con <a3con.noreply@gmail.com>
2024-12-04 18:29:30 +0300

    Move style to own dir

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/app/app'
-import './index.scss'
+import './styles/index.scss'
 import { Provider } from 'react-redux'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
